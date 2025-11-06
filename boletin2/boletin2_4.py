087b052aca9b6b8d8c19ebe63e652b80c90c68bc
Hugo <hperezmeseguer@danielcastelao.org>
2025-11-06 13:41:34 +0100

    ex4 bol2 acabado

---
+++ boletin2/boletin2_4.py
@@ -0,0 +1,9 @@
+num1 = float(input("dime un número:"))
+num2 = float(input("dime un número:"))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print(suma,resta, multiplicacion, division)
