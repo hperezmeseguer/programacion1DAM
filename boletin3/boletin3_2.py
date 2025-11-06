de112a57b8358af6578395c6eaea90a9948700d5
Hugo <hperezmeseguer@danielcastelao.org>
2025-11-06 13:50:16 +0100

    ex2 bol3 acabado

---
+++ boletin3/boletin3_2.py
@@ -0,0 +1,9 @@
+num1 = float(input("Dime un número:"))
+num2 = float(input("Dime un número:"))
+
+if num1 >= num2:
+    resta = num1 - num2
+    print(resta)
+else:
+    suma = num1 + num2
+    print(suma)
